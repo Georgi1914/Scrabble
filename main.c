62889c3200795ef731f11e76f3c3073dcbf4a804
VladiPav <61563439+VladiPav@users.noreply.github.com>
2021-06-13 17:04:10 +0300

    added settings

--- main.c
+++ main.c
@@ -1,34 +1,69 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<conio.h>
 
-void menu(int option){
-    printf("Choose an option:\n");
-    printf("1. Start new game\n");
-    printf("2. Settings\n");
-    printf("3. Enter a new word\n");
-    printf("4. Quit\n");
+void settings(){
 
-    scanf("%d", &option);
+    FILE* file = fopen("./settings.bin", "rb");
 
-    switch(option){
-        case 1: break;
+    int stngs[2];
+    fread(stngs, sizeof(stngs[0]), 2, file);
+    fclose(file);
+
+    printf("Current settings:\n");
+    printf("Rounds: %d\n", stngs[0]);
+    printf("Letters: %d\n", stngs[1]);
+
+    printf("\nChange settings?\n");
+    printf("1. Yes\n");
+    printf("2. No\n");
+
+    unsigned int settings_option;
+    scanf("%d", &settings_option);
+
+    switch(settings_option){
+        case 1:
+            file = fopen("./settings.bin", "wb");
+            printf("Number of rounds: ");
+            scanf("%d", &stngs[0]);
+            printf("Number of letters: ");
+            scanf("%d", &stngs[1]);
+            fwrite(stngs, sizeof(stngs[0]), 2, file);
+            fclose(file);
+            break;
         case 2: break;
-        case 3: break;
-        case 4: 
-            printf("\nHave a nice day!");
-            exit(0);
-        default: 
-            printf("\nWrong input. Try again.");
-            menu(option);
+        default:
+            printf("\nWrong input. Try again.\n");
+            settings();
             break;
     }
 }
 
-int main(){
 
-    unsigned int option;
-    menu(option);
-  
+int main(){
+    while(1){
+        unsigned int option;
+
+        printf("\nChoose an option:\n");
+        printf("1. Start new game\n");
+        printf("2. Settings\n");
+        printf("3. Enter a new word\n");
+        printf("4. Quit\n");
+
+        scanf("%d", &option);
+
+        switch(option){
+            case 1: break;
+            case 2: 
+                settings(); 
+                break;
+            case 3: break;
+            case 4: 
+                printf("Have a nice day!");
+                exit(0);
+            default: 
+                printf("\nWrong input. Try again.\n");
+                break;
+        }
+    }
     return 0;
 }
