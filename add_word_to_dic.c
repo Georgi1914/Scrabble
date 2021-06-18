950f992f73f72542b095bb963c3596382969e6ac
1freshie <hristian.todorov14@gmail.com>
2021-06-18 21:44:40 +0300

    adding a word in the dictionary

--- add_word_to_dic.c
+++ add_word_to_dic.c
@@ -2,53 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct node_t
-{
-  char* name;
-  struct node_t* right; 
-  struct node_t* left; 
-};
-
-// void WriteToFile(char* file_name, struct node_t** binary_tree)
-// {
-//   FILE* file;
-//    if (fopen_s(&file, file_name, "w") != 0)
-//    {
-//      puts("failure\n");
-//    }
-//    else
-//    {
-//     WriteNodes(file, binary_tree);
-//     fclose(file);
-//    }
-// }
-
-// void WriteNodes(FILE* file, struct node_t** binary_tree)
-// {
-//   if(binary_tree != NULL)
-//    {
-//     WriteNodes(file, &(*binary_tree)->left);
-//     //fprintf(file,"%s %d\n",(*binary_tree)->surname,(*binary_tree)->ID_student);
-//     WriteNodes(file,&(*binary_tree)->right);
-//    }
-// }
-
-// // void export(struct node_t* root) {
-// //     FILE* fp;
-// //     fp = fopen("Names.txt", "w");
-// //     export_recursive(fp, root);
-// //     fclose(fp);
-// // }
-
-// // void export_recursive(FILE* fp, struct node_t* root) {
-// //     if (root != NULL) {
-// //         export_recursive(fp, root->left);
-// //         fprintf(fp,root->Emp);
-// //         export_recursive(fp, root->right);
-// //     }
-// // }
-
-
 void add_word_to_dictionary() {
     
     char word[50];
@@ -81,6 +34,3 @@ void add_word_to_dictionary() {
 int main() {
     add_word_to_dictionary();
 }
-
-//spisuk s dannite na txt file
-// vruzkite na tezi danni na drug/sushtiq txt file
